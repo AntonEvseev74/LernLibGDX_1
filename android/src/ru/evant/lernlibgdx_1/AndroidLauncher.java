83b19e134ac08f45d5f6941ef041845919f141d4
Anton Evseev <58934670+AntonEvseev74@users.noreply.github.com>
2021-11-04 17:21:31 +0500

    Самообразование #1 Java Game Development with LibGDX, 2nd Edition

---
+++ android/src/ru/evant/lernlibgdx_1/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.evant.lernlibgdx_1;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.evant.lernlibgdx_1.HelloWorldImage;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HelloWorldImage(), config);
+	}
+}
